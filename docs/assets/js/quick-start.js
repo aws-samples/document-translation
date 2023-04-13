cbf0e71bee104ca223aadfa437b02025580c7088
Philip Whiteside <philipws@amazon.co.uk>
2023-04-13 16:28:41 +0100

    Add quick-start form

---
+++ docs/assets/js/quick-start.js
@@ -0,0 +1,230 @@
+// FORM REPO TAGS
+async function getRepoTags(){
+	const response = await fetch("https://api.github.com/repos/aws-samples/document-translation/tags")
+	const repoTags = await response.json()
+	var options = ""
+	repoTags.forEach(tag => {
+		options += `<option value="${tag.name}">${tag.name}</option>`
+	});
+
+	const sourceGitTag = document.getElementById("sourceGitTag")
+	sourceGitTag.innerHTML = options
+}
+getRepoTags()
+
+// FORM INTERACTION
+const form = document.getElementById('form');
+
+function setElementDisplay(className, newState){
+	const elements = document.getElementsByClassName(className);
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].style.display = newState;
+	}
+}
+
+function setElementRequired(className, newState){
+	const elements = document.getElementsByClassName(className);
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].required = newState;
+	}
+}
+
+function filterFormFields(className, newState){
+	setElementRequired("requiredFor" + className, newState)
+	if (newState){
+		setElementDisplay("isFor" + className, "block")
+		setElementDisplay("isNotFor" + className, "none")
+	} else{
+		setElementDisplay("isFor" + className, "none")
+		setElementDisplay("isNotFor" + className, "block")
+	}
+}
+
+function updateFormFields() {
+	filterFormFields("PiiDetection", form.piiDetectionEnable.checked);
+	filterFormFields("CustomDomain", form.customDomainEnable.checked);
+	filterFormFields("CognitoLocalUsers", form.cognitoLocalUsers.checked);
+	filterFormFields("CognitoSamlUsers", form.cognitoSamlUsers.checked);
+	filterFormFields("Translation", form.translation.checked);
+
+	if (form.sourceGitService.value == "codecommit"){
+		filterFormFields("CodeCommit", true);
+		filterFormFields("GitHub", false);
+	} else if (form.sourceGitService.value == "github"){
+		filterFormFields("CodeCommit", false);
+		filterFormFields("GitHub", true);
+	}
+
+	if (form.cognitoLocalUsers.checked && form.cognitoLocalUsersMfa.value == "required" || form.cognitoLocalUsers.checked && form.cognitoLocalUsersMfa.value == "optional"){
+		filterFormFields("CognitoLocalUsersMfa", true);
+	} else {
+		filterFormFields("CognitoLocalUsersMfa", false);
+	}
+}
+
+form.addEventListener('change', function() {
+	updateFormFields();
+});
+
+// FORM HANLDER
+var output = "";
+function appendSection(description){
+	output += "\n# " + description + "\n"
+}
+function appendStep(description, command){
+	output += command +  " # " + description + "\n"
+}
+
+function handleForm(event) {
+	event.preventDefault();
+	const data = Object.fromEntries(new FormData(event.target).entries())
+
+	appendSection('----------')
+	appendSection('Configuration')
+	if (data.piiDetectionEnable) {
+		appendSection('PII Detection')
+		appendStep('Enable Macie', 'aws macie2 enable-macie')
+		appendStep('Create Macie log group', 'aws logs create-log-group --log-group-name /aws/macie/classificationjobs')
+		appendStep('Enable PII detection', `export translationPii="${data.piiDetectionEnable}"`)
+		appendStep('Set PII lifecycle', `export translationLifecyclePii="${data.piiDetectionLifecycle}"`)
+	};
+
+	if (data.customDomainEnable) {
+		appendSection('Custom Domain')
+		appendStep('Set custom domain name', `export webUiCustomDomain="${data.customDomainName}"`)
+		appendStep('Set custom domain certificate', `export webUiCustomDomain="${data.customDomainCert}"`)
+	};
+	if (data.sourceGitService){
+		switch (data.sourceGitService) {
+			case "codecommit":
+				appendSection('Source Service CodeCommit')
+				appendStep('Set repo name', `export sourceGitRepo="${data.sourceGitRepo}"`)
+				appendStep('Create CodeCommit repo', `aws codecommit create-repository --repository-name ${data.sourceGitRepo}`)
+				break;
+			case "github":
+				appendSection('Source Service GitHub')
+				appendStep('Set source service', `export sourceGitService="${data.sourceGitService}"`)
+				appendStep('Set repo name', `export sourceGitRepo="${data.sourceGitRepoOwner}/${data.sourceGitRepo}"`)
+				break;
+		}
+		appendStep('Set branch name', `export sourceGitBranch="${data.sourceGitBranch}"`)
+	}
+	if (data.cognitoLocalUsers) {
+		appendSection('Cognito Local Users')
+		appendStep('Enable Cognito local users', `export cognitoLocalUsers="${data.cognitoLocalUsers}"`)
+		appendStep('Set MFA enforcement', `export cognitoLocalUsersMfa="${data.cognitoLocalUsersMfa}"`)
+		appendStep('Enable MFA type OTP', `export cognitoLocalUsersMfaOtp="${data.cognitoLocalUsersMfaOtp}"`)
+		appendStep('Enable MFA type SMS', `export cognitoLocalUsersMfaSms="${data.cognitoLocalUsersMfaSms}"`)
+	};
+
+	if (data.cognitoSamlUsers) {
+		appendSection('Cognito SAML Users')
+		appendStep('Enable Cognito SAML users', `export cognitoSamlUsers="${data.cognitoSamlUsers}"`)
+		appendStep('Set SAML metadata URL', `export cognitoSamlMetadataUrl="${data.cognitoSamlMetadataUrl}"`)
+	};
+	if (data.webUi) {
+		appendSection('Web UI')
+		appendStep('Enable Web UI', `export webUi="${data.webUi}"`)
+	};
+	if (data.webUi) {
+		appendSection('Translation')
+		appendStep('Enable Translation', `export translation="${data.translation}"`)
+		appendStep('Set default lifecycle', `export translationLifecycleDefault="${data.translationLifecycleDefault}"`)
+	};
+
+	appendSection('----------')
+	appendSection('Deployment')
+
+	appendSection('CDK Bootstrap')
+	appendStep('Bootstrap the account', `cdk bootstrap aws://${data.accountId}/\${AWS_REGION}`)
+
+	appendSection('Clone source code')
+	appendStep('Clone the upstream project git repository', 'git clone https://github.com/aws-samples/document-translation.git')
+	appendStep('Change directory into the pulled project directory', 'cd document-translation')
+	appendStep('Fetch available tags', 'git fetch --all --tags')
+	appendStep('View tags', 'git tag')
+	appendStep('Checkout tag', `git checkout tags/${data.sourceGitTag}`)
+
+	appendSection('Push source code to your CodeCommit')
+	appendStep('Enable the AWS CLI git credentials helper', `git config --global credential.helper '!aws codecommit credential-helper $@'`)
+	appendStep('Enable the AWS CLI git credentials helper', 'git config --global credential.UseHttpPath true')
+	appendStep('Add CodeCommit as a remote', `git remote add codecommit https://git-codecommit.\${AWS_REGION}.amazonaws.com/v1/repos/${data.sourceGitRepo}`)
+	appendStep('Push files', `git push codecommit ${data.sourceGitBranch}`)
+
+	appendSection('Deploy the pipeline ')
+	appendStep('Change directory into the infrastructure directory', `cd infrastructure`)
+	appendStep('Install node dependencies', `npm install`)
+	appendStep('Deploy', `cdk deploy`)
+
+	const result = document.getElementById('result');
+
+	result.innerText = output;
+	form.style.display = "none";
+	result.style.display = "block";
+	console.log(output);
+}
+form.addEventListener('submit', handleForm);
+
+// FORM STYLE
+const styles = `
+	/* LAYOUT */
+	form .formOptionGroup .formOptionLabel {
+		text-align: right;
+	}
+	form .formOptionGroup .formOptionLabel,
+	form .formOptionGroup .formOptionInput {
+		display: inline-block;
+		margin: 0;
+		box-sizing: border-box;
+	}
+	form .formOptionGroup .formOptionLabel{
+		padding: 0 1rem;
+		margin: 0 1rem 0 0;
+		width: 33%;
+		min-width: 33%;
+		border-right: solid 1px black;
+		margin-bottom: 0.5rem;
+	}
+	form .formOptionGroup .formOptionInput {
+		width: calc(66% - 1rem);
+		min-width: calc(66% - 1rem);
+	}
+
+	/* STYLE */
+	form input {
+		border: 1px solid;
+		border-radius: 0.25rem;
+	}
+	form button {
+		margin: 1rem !important;
+	}
+	form input[type="checkbox"] {
+		height: 1rem;
+		vertical-align: middle;
+		width: 1rem !important;
+		min-width: 1rem !important;
+		max-width: 1rem !important;
+	}
+	form fieldset {
+		border-width: 1px 0 0 0;
+		margin-bottom: 1rem;
+	}
+	form fieldset legend {
+		font-weight: bold;
+	}
+	form fieldset legend .info {
+		font-weight: lighter;
+		font-size: 0.75rem;
+	}
+	
+	/* VALIDATION */
+	form input:invalid {
+		border-color: #ff9e9e;
+	}
+	input:valid {
+		border-color: #67e867;
+	}
+`
+const styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
--- docs/docs/quick-start.md
+++ docs/docs/quick-start.md
@@ -9,3 +9,127 @@ has_children: false
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 -->
+
+This project is [configurable]({{ site.baseurl }}/docs/architecture/features) to suit your specific requirements. For a full explanation of the various options please review the [full installation guide]({{ site.baseurl }}/docs/installation/). If an option is unclear to you please refer to the full guide.
+
+This form will collate your inputs and return the appropriate commands to enter for deployment. The output can be used with the [CloudShell]({{ site.baseurl }}/docs/installation/prerequisite/cloudshell) to quickly perform the installation. Please review the output to ensure you are happy with the values.
+
+<code id="result" style="display: none;"></code>
+<form id="form">
+	<fieldset>
+		<legend>General Information</legend>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="accountId">AWS Account ID</label>
+			<input class="formOptionInput requiredForGitHub" type="text" name="accountId" placeholder="123456789012" maxlength="12" minlength="12" pattern="\d+" required/>
+		</div>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="sourceGitTag">Git Release Tag</label>
+			<select id="sourceGitTag" class="formOptionInput" name="sourceGitTag">
+                <option value="v1.1.2">v1.1.2</option>
+			</select>
+		</div>
+	</fieldset>
+	<fieldset>
+		<legend>Source Service <a class="info" target="_blank" href="{{ site.baseurl }}/docs/installation/source-service/">(info)</a></legend>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="sourceGitService">Service</label>
+			<select class="formOptionInput" name="sourceGitService">
+				<option value="codecommit">AWS CodeCommit</option>
+				<option value="github">GitHub</option>
+			</select>
+		</div>
+		<div class="formOptionGroup isForGitHub isNotForCodeCommit" style="display: none;">
+			<label class="formOptionLabel" for="sourceGitRepoOwner">Repo Owner</label>
+			<input class="formOptionInput requiredForGitHub" type="text" name="sourceGitRepoOwner" placeholder="username">
+		</div>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="sourceGitRepo">Repo Name</label>
+			<input class="formOptionInput" type="text" name="sourceGitRepo" placeholder="document-translation" required>
+		</div>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="sourceGitBranch">Branch Name</label>
+			<input class="formOptionInput" type="text" name="sourceGitBranch" placeholder="main" required/>
+		</div>
+	</fieldset>
+	<fieldset>
+		<legend>Web UI <a class="info" target="_blank" href="{{ site.baseurl }}/docs/installation/configuration/options.html#enable-web-ui">(info)</a></legend>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="webUi">Enable</label>
+			<input class="formOptionInput" type="checkbox" name="webUi" checked/>
+		</div>
+	</fieldset>
+	<fieldset>
+		<legend>Translation <a class="info" target="_blank" href="{{ site.baseurl }}/docs/installation/configuration/options.html#translation--translation-pii">(info)</a></legend>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="translation">Enable</label>
+			<input class="formOptionInput" type="checkbox" name="translation" checked/>
+		</div>
+		<div class="formOptionGroup isForTranslation">
+			<label class="formOptionLabel" for="translationLifecycleDefault">Expire files in days</label>
+			<input class="formOptionInput requiredForTranslation" type="number" name="translationLifecycleDefault" min="1" placeholder="7" required/>
+		</div>
+	</fieldset>
+	<fieldset>
+		<legend>PII Detection <a class="info" target="_blank" href="{{ site.baseurl }}/docs/installation/configuration/options.html#translation--translation-pii">(info)</a></legend>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="piiDetectionEnable">Enable</label>
+			<input class="formOptionInput" type="checkbox" name="piiDetectionEnable" checked/>
+		</div>
+		<div class="formOptionGroup isForPiiDetection">
+			<label class="formOptionLabel" for="piiDetectionLifecycle">Expire PII files in days</label>
+			<input class="formOptionInput requiredForPiiDetection" type="number" name="piiDetectionLifecycle" min="1" placeholder="3" required/>
+		</div>
+	</fieldset>
+	<fieldset>
+		<legend>Custom Domain <a class="info" target="_blank" href="{{ site.baseurl }}/docs/installation/prerequisite/domain.html">(info)</a></legend>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="customDomainEnable">Enable</label>
+			<input class="formOptionInput" type="checkbox" name="customDomainEnable"  checked/>
+		</div>
+		<div class="formOptionGroup isForCustomDomain">
+			<label class="formOptionLabel" for="customDomainName">Domain Name</label>
+			<input class="formOptionInput requiredForCustomDomain" type="text" name="customDomainName" placeholder="document-translation.business.com" required/>
+		</div>
+		<div class="formOptionGroup isForCustomDomain">
+			<label class="formOptionLabel" for="customDomainCert">Certificate ARN</label>
+			<input class="formOptionInput requiredForCustomDomain" type="text" name="customDomainCert" placeholder="arn:aws:acm:us-east-1:123456789012:certificate/abcdefgh-1234-5678-9012-ijklmnopqrst" required/>
+		</div>
+	</fieldset>
+	<fieldset>
+		<legend>Cognito SAML Users <a class="info" target="_blank" href="{{ site.baseurl }}/docs/installation/configuration/options.html#enable-cognito-saml-provider-users">(info)</a></legend>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="cognitoSamlUsers">Enable</label>
+			<input class="formOptionInput" type="checkbox" name="cognitoSamlUsers" checked/>
+		</div>
+		<div class="formOptionGroup isForCognitoSamlUsers">
+			<label class="formOptionLabel" for="cognitoSamlMetadataUrl">Metadata URL</label>
+			<input class="formOptionInput requiredForCognitoSamlUsers" type="text" name="cognitoSamlMetadataUrl" placeholder="https://domain.tld/path/to/metadata.xml?appid=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" required/>
+		</div>
+	</fieldset>
+	<fieldset>
+		<legend>Cognito Local Users <a class="info" target="_blank" href="{{ site.baseurl }}/docs/installation/configuration/options.html#enable-cognito-local-users">(info)</a></legend>
+		<div class="formOptionGroup">
+			<label class="formOptionLabel" for="cognitoLocalUsers">Enable</label>
+			<input class="formOptionInput" type="checkbox" name="cognitoLocalUsers"/>
+		</div>
+		<div class="formOptionGroup isForCognitoLocalUsers" style="display: none;">
+			<label class="formOptionLabel" for="cognitoLocalUsersMfa">MFA</label>
+			<select class="formOptionInput" name="cognitoLocalUsersMfa">
+				<option value="required">Required</option>
+				<option value="optional">Optional</option>
+				<option value="off">Off</option>
+			</select>
+		</div>
+		<div class="formOptionGroup isForCognitoLocalUsersMfa" style="display: none;">
+			<label class="formOptionLabel" for="cognitoLocalUsersMfaOtp">MFA OTP?</label>
+			<input class="formOptionInput" type="checkbox" name="cognitoLocalUsersMfaOtp"/>
+		</div>
+		<div class="formOptionGroup isForCognitoLocalUsersMfa" style="display: none;">
+			<label class="formOptionLabel" for="cognitoLocalUsersMfaSms">MFA SMS?</label>
+			<input class="formOptionInput" type="checkbox" name="cognitoLocalUsersMfaSms"/>
+		</div>
+	</fieldset>
+    <button type="submit" class="btn btn-blue">Generate</button>
+</form>
+
+<script src="{{ site.baseurl }}/assets/js/quick-start.js"></script>
