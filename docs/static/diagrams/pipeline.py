4a5d45d37d9cb96820df8f811e942646d8609289
Philip Whiteside <philipws@amazon.co.uk>
2024-03-15 10:36:05 +0000

    Pipeline  specific overview diagram

---
+++ docs/static/diagrams/pipeline.py
@@ -0,0 +1,28 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+import os
+from dataclasses import replace
+from diagrams import Diagram, Cluster
+from diagrams.aws.devtools import Codecommit, Codebuild, CloudDevelopmentKit
+from diagrams.aws.management import Cloudformation
+
+import attr
+
+name = os.path.splitext(os.path.basename(__file__))[0]
+name_human = name.replace("_", " ").title()
+
+with Diagram(name_human, filename=name, show=False, graph_attr=attr.graph):
+
+    #
+    # PIPELINE
+    # PIPELINE | NODES
+    with Cluster("AWS CodePipeline"):
+        code_commit = Codecommit("AWS CodeCommit\n(Git Repo)")
+        code_build = Codebuild("AWS CodeBuild\n(CI)")
+        code_cdk = CloudDevelopmentKit("AWS Cloud Development Kit \n(IaC)")
+
+    code_commit >> code_build >> code_cdk
+
+    cfn = Cloudformation("AWS CloudFormation\n(App Stack)")
+    code_cdk >> cfn
