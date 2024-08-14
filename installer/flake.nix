803e694bf84df41e9aef745003fcd7c2128ff9eb
Philip Whiteside <philipws@amazon.co.uk>
2024-08-14 15:06:17 +0100

    Dev env

---
+++ installer/flake.nix
@@ -0,0 +1,21 @@
+{
+	description = "Installer Development";
+	
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
+
+	outputs = {
+		self,
+		nixpkgs,
+	}: let
+		pkgs = import nixpkgs { system ="x86_64-linux"; };
+	in {
+		devShells.x86_64-linux.default = pkgs.mkShell {
+			packages = with pkgs; [
+				nodejs_22
+				# Optional
+				bat
+			];
+			shellHook = ''bat --style=plain --paging=never ./README.md''; 
+		};
+	};
+}
